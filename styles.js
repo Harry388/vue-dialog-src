4e177d094c06eee8aae374d9d10cce63a6b1821f
Harry388 <harrythompson3765@gmail.com>
2022-07-27 14:47:26 +0100

    z-index update

--- styles.js
+++ styles.js
@@ -5,7 +5,7 @@ const applyStyles = options => {
     style.appendChild(document.createTextNode(`
     ${options.wrapper ? `.__dialog-wrapper {
         position: fixed;
-        z-index: 1;
+        z-index: 10;
         left: 0;
         top: 0;
         width: 100%;
